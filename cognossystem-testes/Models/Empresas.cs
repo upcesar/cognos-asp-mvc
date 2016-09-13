363fdeff22a400f1f034eeb5ad8036f25e23ea17
César M. Urdaneta <upcesar@gmail.com>
2016-09-13 19:05:31 -0300

    Delete contact and About (Controller, View and TestMethod). Adjusting view Header 'Empresas'

--- cognossystem-testes/Models/Empresas.cs
+++ cognossystem-testes/Models/Empresas.cs
@@ -13,7 +13,7 @@ namespace cognossystem_testes.Models
         public string Site { get; set; }
         public Status Status { get; set; }
         public DateTime Data_Inclusao { get; set; }
-        public DateTime ? Data_Ultima_alteracao { get; set; }
+        public DateTime ? Data_Ultima_Alteracao { get; set; }
         public virtual List<Contatos> Contatos { get; set; }
     }
 }
